e01d916c6d99e3653a900de2dba06bdf9a7a36f2
andersonAzzolinii <anderson.azzolinii@gmail.com>
2025-07-20 20:33:48 -0300

    - created initial server file

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
+
+export default app;
