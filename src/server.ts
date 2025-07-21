9f0c72c27596ec8faf3256b8786d55613149dc77
andersonAzzolinii <anderson.azzolinii@gmail.com>
2025-07-20 21:25:39 -0300

    - created database connection file and create connection before of start the server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import database from './config/database';
 
 dotenv.config();
 
@@ -9,8 +10,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+async function connectDB() {
+  try {
+    await database.connect();
+  } catch (error) {
+    console.error('Falha ao iniciar o servidor:', error);
+    process.exit(1);
+  }
+}
+
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
-
 export default app;
